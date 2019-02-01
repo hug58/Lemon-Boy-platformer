2677d12fb1ff26c7115e82425fc8664f3c175820
Hug58 <hugomontaez@gmail.com>
2019-01-31 22:57:45 -0400

    Delete block.tsx

--- map/block.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="vilenes" tilewidth="16" tileheight="16" tilecount="1920" columns="48">
- <image source="../../tmx/img/vilenes.png" trans="ff00ff" width="768" height="640"/>
-</tileset>
