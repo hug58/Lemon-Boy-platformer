dabbc9f0ed95b7d87f58e1ef60d6ab295560c364
Hug58 <hugomontaez@gmail.com>
2019-01-31 23:00:07 -0400

    Add files via upload

--- map/pasto.tsx
+++ map/pasto.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="pasto" tilewidth="20" tileheight="20" tilecount="1" columns="1">
- <image source="../image/pasto.png" trans="ff00ff" width="20" height="20"/>
+<tileset name="pasto" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../image/pasto.png" trans="ff00ff" width="96" height="96"/>
 </tileset>
---
+++ map/spikes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spikes" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+ <image source="../image/spikes.png" width="256" height="32"/>
+</tileset>
