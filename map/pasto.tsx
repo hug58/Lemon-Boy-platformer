a20388023ac9849320a87a4e4821e9399f372826
hug58 <hugomontaez@gmail.com>
2019-02-22 20:16:38 -0400

    nuevo tileset piso

--- map/pasto.tsx
+++ map/pasto.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="pasto" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../image/pasto.png" width="96" height="96"/>
+<tileset name="pasto" tilewidth="32" tileheight="32" tilecount="48" columns="4">
+ <image source="../image/pasto.png" trans="ffff00" width="128" height="96"/>
 </tileset>
